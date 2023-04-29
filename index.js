f8a2794a6aec072877faebd0ea69cea0f0a6c8ae
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 10:25:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Step 1: Sort the input array in ascending order.
+  array.sort((a, b) => a - b);
+
+  // Step 2: Initialize left and right pointers at the beginning and end of the array, respectively.
+  let left = 0;
+  let right = array.length - 1;
+
+  // Step 3: While left < right:
+  while (left < right) {
+    // Step 3a: Calculate the sum of the numbers at positions left and right.
+    const sum = array[left] + array[right];
+
+    // Step 3b: If the sum is equal to the target, return true.
+    if (sum === target) {
+      return true;
+    }
+
+    // Step 3c: If the sum is less than the target, increment left.
+    if (sum < target) {
+      left++;
+    }
+
+    // Step 3d: If the sum is greater than the target, decrement right.
+    if (sum > target) {
+      right--;
+    }
+  }
+
+  // Step 4: If no two numbers in the array add up to the target, return false.
+  return false;
 }
+
+module.exports = hasTargetSum;
 
 /* 
   Write the Big O time complexity of your function here
+  O(nlogn) 
 */
 
 /* 
